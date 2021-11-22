2878cf683bbdad8afac2c197546c5c4ec7c32d55
Masahito Osako <velonica.gale2227@gmail.com>
2021-11-22 17:31:02 +0900

    update

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ const bcrypt = require('bcrypt')
 const User = require('./schema/User')
 const Message = require('./schema/Message')
 
+const MessagesController = require('./controllers/MessagesController')
+
 const SALT_ROUNDS = 10
 
 const app = express()
@@ -129,36 +131,8 @@ passport.deserializeUser((id, done) => {
   })
 })
 
-app.get('/update', (_req, res, _next) => {
-  return res.render('update')
-})
-
-app.post('/update', fileupload(), (req, res, next) => {
-  User.findById(req.session.passport.user, (err, user) => {
-    if (err) next(err)
-
-    let messageParams = {
-      user: user._id,
-      message: req.body.message
-    }
-
-    if (req.files && req.files.image) {
-      const image = req.files.image
-      image.mv('./image/' + image.name, err => {
-        if (err) throw err
-        messageParams.image_path = '/image/' + image.name
-      })
-    }
-
-    const newMessage = new Message(messageParams)
-
-    newMessage.save(createErr => {
-      if (createErr) throw createErr
-
-      return res.redirect('/')
-    })
-  })
-})
+app.get('/messages/new', MessagesController.index)
+app.post('/messages', fileupload(), MessagesController.create)
 
 const server = http.createServer(app)
 server.listen(3000)
