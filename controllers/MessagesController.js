66d6142f4704978b9095a762ad28814da429f1dc
Masahito Osako <velonica.gale2227@gmail.com>
2021-11-22 17:24:46 +0900

    implement messages controller

---
+++ controllers/MessagesController.js
@@ -0,0 +1,34 @@
+const User = require('../schema/User')
+const Message = require('../schema/Message')
+
+module.exports = {
+  index: (_req, res, _next) => {
+    return res.render('messages/index')
+  },
+  create: (req, res, next) => {
+    User.findById(req.session.passport.user, (err, user) => {
+      if (err) next(err)
+
+      let messageParams = {
+        user: user._id,
+        message: req.body.message
+      }
+
+      if (req.files && req.files.image) {
+        const image = req.files.image
+        image.mv('./image/' + image.name, err => {
+          if (err) throw err
+          messageParams.image_path = '/image/' + image.name
+        })
+      }
+
+      const newMessage = new Message(messageParams)
+
+      newMessage.save(createErr => {
+        if (createErr) throw createErr
+
+        return res.redirect('/')
+      })
+    })
+  }
+}
